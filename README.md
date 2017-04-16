53ce935404ebe0df92aeb931a753108d1bee4307
spelley <stevenpelley@gmail.com>
2017-04-16 08:51:00 -0700

    adding skeleton for real waffle warehouse app
    Includes tabs for different stations, prototypes of radio selects and
    event history.

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 The waffle warehouse service.
 
-At the moment this contains a ReactJS app that, when a button is pressed, queries the server for the current time and then displays the time.  The root site (the React app) is itself served from the same server.
+At the moment this contains a ReactJS app that, when a button is pressed,
+queries the server for the current time and then displays the time.  The root
+site (the React app) is itself served from the same server.
+
+See the Makefile for basic structure.  Make builds both the ReactJS and golang
+components.
+
+Run as ./waffle-warehouse
+Browse localhost:1323
--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { RadioGroup, RadioButton } from 'react-radio-buttons';
+import ReactList from 'react-list';
 
 class TimeButton extends Component {
   render() {
@@ -16,7 +19,85 @@ class TimeText extends Component {
   }
 }
 
-class App extends Component {
+class WaffleSelector extends Component {
+  constructor() {
+    super()
+    this.state = {
+      whichWaffle : 1,
+      lastWaffle : 'asdf',
+    };
+  }
+
+  setNumWaffles(arg) {
+    this.setState({whichWaffle : arg});
+  }
+
+  onWafflesDone() {
+    var obj = {
+      selectedWaffles : this.state['whichWaffle'],
+      timeMs : new Date().getTime(),
+    }
+    console.log(JSON.stringify(obj))
+    this.setState({lastWaffle : obj});
+  }
+
+  render() {
+    return (
+      <div className="WaffleSelector">
+      <RadioGroup onChange={(arg) => this.setNumWaffles(arg)} horizontal>
+        <RadioButton value="1">1</RadioButton>
+        <RadioButton value="2">2</RadioButton>
+        <RadioButton value="3">3</RadioButton>
+        <RadioButton value="4">4</RadioButton>
+      </RadioGroup>
+      <button className="makeWaffles" onClick={() => this.onWafflesDone()}>Waffles Done</button>
+      <div className="waffleValue">{this.state.whichWaffle}</div>
+      <div className="lastWaffle">lastWaffle: {JSON.stringify(this.state.lastWaffle)}</div>
+      </div>
+    )
+  }
+}
+
+class WaffleEvents extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events : [],
+    };
+  }
+
+  createTimeEvent() {
+    fetch('http://localhost:1323/waffle/time') 
+      .then(result => result.text())
+      .then(text => this.addEvent(text))
+  }
+
+  addEvent(e) {
+    var newEvents = this.state.events.slice();
+    newEvents.unshift(e)
+    this.setState({events : newEvents});
+  }
+
+  renderItem(index, key) {
+    return <div key={key}>{JSON.stringify(this.state.events[index])}</div>;
+  }
+
+  render() {
+    return (
+      <div className="WaffleEvents">
+      <button className="WaffleEventButton" onClick={() => this.createTimeEvent()}>add event</button>
+      <div className="WaffleEventsScroll" style={{overflow:'auto', maxHeight: 100}}>
+      <ReactList
+        itemRenderer={(index, key) => this.renderItem(index, key)}
+        length={this.state.events.length}
+      />
+      </div>
+      </div>
+    );
+  }
+}
+
+class WafflePanel extends Component {
   constructor() {
     super()
     this.state = {
@@ -26,7 +107,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="WafflePanel">
+      <WaffleSelector />
+      <WaffleEvents />
       <TimeButton onClick={() => this.updateTime()} />
       <TimeText value={this.state.value} />
       </div>
@@ -37,6 +120,31 @@ class App extends Component {
     fetch('http://localhost:1323/waffle/time') 
       .then(result => result.text())
       .then(text => this.setState({value : text}))
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+
+      <Tabs>
+        <TabList>
+          <Tab>Waffles</Tab>
+          <Tab>Eggs</Tab>
+          <Tab>Hash Browns</Tab>
+        </TabList>
+
+        <TabPanel>
+          <WafflePanel />
+        </TabPanel>
+        <TabPanel>
+          <h2>Hello from Eggs</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Hello from Hash Browns</h2>
+        </TabPanel>
+      </Tabs>
+    );
   }
 }
 
